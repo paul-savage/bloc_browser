48bc93c8bb286fe2a7e4355063f1bc74c79fb366
Paul Savage <p.a.savage@btinternet.com>
2015-09-28 12:36:17 +0100

    Assignment 23 completed

--- BlocBrowser/BlocBrowser/ViewController.m
+++ BlocBrowser/BlocBrowser/ViewController.m
@@ -37,7 +37,7 @@
     self.textField.returnKeyType = UIReturnKeyDone;
     self.textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
     self.textField.autocorrectionType = UITextAutocorrectionTypeNo;
-    self.textField.placeholder = NSLocalizedString(@"Website URL", @"Placeholder text for web browser URL field");
+    self.textField.placeholder = NSLocalizedString(@"Website URL, or search terms with space", @"Placeholder text for web browser URL field");
     self.textField.backgroundColor = [UIColor colorWithWhite:220/255.0f alpha:1];
     self.textField.delegate = self;
     
@@ -108,14 +108,21 @@
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
     
+    NSURL *URL;
+    
     [textField resignFirstResponder];
     
     NSString *URLString = textField.text;
     
-    NSURL *URL = [NSURL URLWithString:URLString];
+    if ([URLString containsString:@" "]) {
+        
+        URLString = [NSString stringWithFormat:@"http://www.google.com/search?q=%@", [URLString stringByReplacingOccurrencesOfString:@" " withString:@"+" ]];
+   }
+    
+    URL = [NSURL URLWithString:URLString];
     
     if (!URL.scheme) {
-        // The user didn't type http: or https:
+            // The user didn't type http: or https:
         URL = [NSURL URLWithString:[NSString stringWithFormat:@"http://%@", URLString]];
     }
     
@@ -123,7 +130,7 @@
         NSURLRequest *request = [NSURLRequest requestWithURL:URL];
         [self.webView loadRequest:request];
     }
-    
+   
     return NO;
 }
 
